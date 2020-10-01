4dba3da90143c943cf4b669461fbe5b3781adc37
dwericsonx27 <dwericsonx27@gmail.com>
2020-09-30 21:16:42 -0400

    Added more tests for integer root.

--- c++/cpp/perfect.cpp
+++ c++/cpp/perfect.cpp
@@ -1,9 +1,13 @@
 #include <cmath>
+#include <stdexcept>
 
 int intSquareRoot(int value)
 {
     // Calculates the integer square root of an integer.
-
+    if (value < 0)
+    {
+        throw std::out_of_range("Out of range");
+    }
     return int(sqrt(value));
 }
 
--- c++/test/testPerfect.cpp
+++ c++/test/testPerfect.cpp
@@ -1,9 +1,21 @@
 #include <gtest/gtest.h>
 #include <perfect.h>
 
-TEST(PerfectTest, TestIntSquareRoot){
-  ASSERT_EQ(3, intSquareRoot(9));
-  ASSERT_NE(3, intSquareRoot(8));
+TEST(PerfectTest, TestIntSquareRoot) {
+  ASSERT_EQ(2, intSquareRoot(4));
+  ASSERT_EQ(1, intSquareRoot(1));
+  ASSERT_EQ(0, intSquareRoot(0));
+  ASSERT_EQ(2, intSquareRoot(5));
+  ASSERT_EQ(1, intSquareRoot(3));
+  try {
+    ASSERT_EQ(1, intSquareRoot(-3));
+  }
+  catch(std::out_of_range const & err) {
+    EXPECT_EQ(err.what(),std::string("Out of range"));
+  }
+  catch(...) {
+    FAIL() << "Expected std::out_of_range";
+  }
 }
 
 TEST(PerfectTest, TestPerfectSquare){
